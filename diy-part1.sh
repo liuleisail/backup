0a89976e6d979f55ddd0dda52db6c31d50f9ed96
liuleisail <30498867+liuleisail@users.noreply.github.com>
2020-04-26 19:28:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,7 @@
 
 # fw876/helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Lienol/openwrt-package
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
